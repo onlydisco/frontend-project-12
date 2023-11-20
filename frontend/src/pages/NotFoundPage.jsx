0434aaf2c0cc7fe48beef137ccae74402d36f633
Сергей Мухин <onlydisco@yandex.ru>
2023-11-20 19:23:17 +0500

    feat: add NotFoundpage

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
-const NotFoundPage = () => <Container fluid>NotFoundPage</Container>;
+const NotFoundPage = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Container className="text-center">
+      <h1 className="h4 text-muted">{t('notFoundPage.header')}</h1>
+      <p className="text-muted">
+        Но вы можете перейти
+        {' '}
+        <Link to="/">на главную страницу</Link>
+      </p>
+    </Container>
+  );
+};
 
 export default NotFoundPage;
